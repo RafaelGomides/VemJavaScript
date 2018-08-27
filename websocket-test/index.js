277b3ecf0a029e941f009627498fca5ec803a4a7
Rafael Gomides <mrrafagomides@gmail.com>
2018-08-27 18:55:40 -0300

    Ultimas Atualizações

--- websocket-test/index.js
+++ websocket-test/index.js
@@ -14,7 +14,13 @@ app.use(express.static('public'));
 var io = socket(server);
 io.on('connection', (socket) => {
     console.log('made socket connection', socket.id);
+
+// Handle Chat event
     socket.on('chat', function(data) {
        io.sockets.emit("chat", data);
-    })
+    });
+
+    socket.on('typing', function(data) {
+       socket.broadcast.emit('typing', data);
+    });
 });
--- websocket-test/public/chat.js
+++ websocket-test/public/chat.js
@@ -5,7 +5,8 @@ var socket = io.connect('http://localhost:4000');
 var message = document.getElementById('message');
    handle = document.getElementById('handle'),
    btn = document.getElementById('send'),
-   output = document.getElementById('output');
+   output = document.getElementById('output'),
+   feedback = document.getElementById('feedback');
 
 // Emit events
 btn.addEventListener('click', function(){
@@ -15,7 +16,16 @@ btn.addEventListener('click', function(){
    });
 });
 
+message.addEventListener('keyup', function() {
+   socket.emit('typing', handle.value);
+});
+
 // Listen from events
 socket.on('chat', function(data) {
+   feedback.innerHTML = "";
    output.innerHTML += '<p><strong>'+data.handle +': </strong>'+ data.message + '</p>';
+});
+
+socket.on('typing', function(data) {
+   feedback.innerHTML = '<p><em>'+data+' está destruindo o próprio teclado...</em></p> '
 });
