8d4e838810bfa21a51a13b5ba621c3ad6a8237f9
Rafael Gomides <mrrafagomides@gmail.com>
2018-09-15 20:55:12 -0300

    Test pt3

--- alura/devNodejs/nodejs/test/books.js
+++ alura/devNodejs/nodejs/test/books.js
@@ -1,21 +1,11 @@
-const http = require('http');
-const assert = require('assert');
+const express = require("../config/express")();
+const request = require('supertest')(express);
 
 describe('#BooksController', function() {
     it('#listagem', function(done) {
-        const conf = {
-            hostname: 'localhost',
-            port: 3000,
-            path: '/produtos',
-            headers: {
-                'Accept': 'application/json'
-            }
-        };
-
-        http.get(conf, function(res) {
-            assert.equal(res.statusCode, 200);
-            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
-            done();
-        });
+        request.get('/produtos')
+            .set('Accept', 'application/json')
+            .expect('content-type', /json/)
+            .expect(200, done);
     });
 });
