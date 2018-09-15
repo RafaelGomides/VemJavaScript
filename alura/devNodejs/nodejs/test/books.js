1b3697e210ab9b2fc2805b694465dcf5a8d885c1
Rafael Gomides <mrrafagomides@gmail.com>
2018-09-15 20:42:39 -0300

    Test pt2

--- alura/devNodejs/nodejs/test/books.js
+++ alura/devNodejs/nodejs/test/books.js
@@ -1,19 +1,21 @@
 const http = require('http');
+const assert = require('assert');
 
+describe('#BooksController', function() {
+    it('#listagem', function(done) {
+        const conf = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
 
-describe('#BooksController', function(done) {
-    const conf = {
-        hostname: 'localhost',
-        port: 3000,
-        path: '/produtos',
-        headers: {
-            'Accept': 'application/json'
-        }
-    };
-
-    http.get(conf, function(res) {
-        if (res.statusCode == 200) console.log("STATUS OK");
-        if (res.headers['content-type'] == 'application/json; charset=utf-8') console.log("HEADER OK");
-        done();
+        http.get(conf, function(res) {
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
+            done();
+        });
     });
 });
