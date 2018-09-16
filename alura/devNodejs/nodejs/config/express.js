6380cb8cb76c7f14b576eec42e312ffbb2a6bedf
Rafael Gomides <mrrafagomides@gmail.com>
2018-09-16 14:50:32 -0300

    Aula9

--- alura/devNodejs/nodejs/config/express.js
+++ alura/devNodejs/nodejs/config/express.js
@@ -14,9 +14,23 @@ module.exports = function() {
     app.use(bodyParser.json());
     app.use(expressValidator());
 
+
     load('routes', {
         cwd: 'app'
     }).then('db').into(app);
+
+    app.use(function(req, res, next) {
+        res.status(404).render('error/404');
+        next();
+    });
+
+    app.use(function(error, req, res, next) {
+        if (process.env.NODE_ENV == 'production') {
+            res.status(500).render('error/500');
+            return;
+        }
+        next(error);
+    });
 
     return app;
 }
